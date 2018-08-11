c1ff56d3c0a2b69efbe5237aed60e276252345a3
yukitamura <yuki_tamura@diveintocode.jp>
2018-08-11 22:34:00 +0900

    sign_up文法ミス修正、およびtop画面からsign_upへのリンク実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, presence: true, length:{maximum:255}
-  validates :password, presence: true,  length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :email, presence: true, length:{maximum:255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :password, presence: true,  length: { maximum: 30, minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180811115100) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -20,13 +29,6 @@ ActiveRecord::Schema.define(version: 20180811115100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
