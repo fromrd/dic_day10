8033213b7f4001e68b3e08abe6760f9ecf675bc0
yukitamura <yuki_tamura@diveintocode.jp>
2018-08-13 19:53:47 +0900

    URLをsign_up,sign_in,mypageに変更、アカウント編集機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
-        redirect_to user_path(user.id)
+        redirect_to mypage_path(id: user.id)
       else
         flash.now[:danger] = 'ログインに失敗しました'
-        render'new'
+        render :new
       end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only: [:edit, :show, :update]
+before_action :require_login, only: [:show, :edit]
   def new
     @user = User.new
   end
@@ -7,18 +8,38 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path(@user.id), notice: "登録しました"
+      redirect_to users_sign_in_path(@user.id), notice: "登録しました"
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+   if @user.update(user_params)
+     redirect_to user_path(@user), notice: "ユーザ登録を編集しました"
+   else
+     render :edit
+   end
+ end
 
    private
      def user_params
        params.require(:user).permit(:email, :password, :password_confirmation)
+     end
+
+     def set_user
+       @user = User.find(params[:id])
+     end
+
+     def require_login
+       unless logged_in? && @current_user.id ==  @user.id
+         redirect_to users_sign_in_path
+       end
      end
 end
